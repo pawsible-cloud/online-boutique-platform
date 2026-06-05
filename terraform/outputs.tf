fc1e668174e2764aef22547f8a780a3c18f087a2
Vivian Okose <vivianokose@gmail.com>
2026-06-05 15:14:43 +0100

    Add monitoring configuration files

---
+++ terraform/outputs.tf
@@ -0,0 +1,22 @@
+# terraform/outputs.tf
+
+output "cluster_endpoint" {
+  description = "EKS cluster API endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data"
+  value       = module.eks.cluster_certificate_authority_data
+  sensitive   = true
+}
+
+output "ecr_registry" {
+  description = "ECR registry URL"
+  value       = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com"
+}
