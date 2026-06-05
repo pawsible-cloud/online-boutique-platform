ed202ff8724a48d3b9430fd08a1f9455a5915743
Vivian Okose <vivianokose@gmail.com>
2026-06-05 15:14:43 +0100

    Add monitoring configuration files

---
+++ terraform/vpc.tf
@@ -0,0 +1,32 @@
+# terraform/vpc.tf
+# Creates a VPC with public and private subnets across 2 availability zones
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "${var.cluster_name}-vpc"
+  cidr = "10.0.0.0/16"
+
+  # Two availability zones for high availability
+  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true   # Use one NAT gateway to save money
+  enable_dns_hostnames   = true
+
+  # Tags required by EKS
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
+  tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    Project = "online-boutique"
+  }
+}
